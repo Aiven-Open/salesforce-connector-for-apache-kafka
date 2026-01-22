1d17cbb700e7b88da5a87f690ab4a15526ca5a80
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2026-01-22 10:34:23 +0000

    Update the configFragment to use the setter
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- salesforce-common/src/main/java/io/aiven/kafka/connect/salesforce/common/config/SalesforceConfigFragment.java
+++ salesforce-common/src/main/java/io/aiven/kafka/connect/salesforce/common/config/SalesforceConfigFragment.java
@@ -16,6 +16,7 @@
 package io.aiven.kafka.connect.salesforce.common.config;
 
 import io.aiven.commons.kafka.config.ExtendedConfigKey;
+import io.aiven.commons.kafka.config.fragment.AbstractFragmentSetter;
 import io.aiven.commons.kafka.config.fragment.ConfigFragment;
 import io.aiven.commons.kafka.config.fragment.FragmentDataAccess;
 import org.apache.kafka.common.config.ConfigDef;
@@ -38,7 +39,7 @@ public class SalesforceConfigFragment extends ConfigFragment {
 	 * The maximum number of records that can be retrieved from a salesforce bulk
 	 * query at a time.
 	 */
-	public static final String SALESFORCE_MAX_RECORDS = "max.records";
+	private static final String SALESFORCE_MAX_RECORDS = "max.records";
 	/**
 	 * The default maximum number of records that can be retrieved from the bulk api
 	 * in one go
@@ -49,7 +50,7 @@ public class SalesforceConfigFragment extends ConfigFragment {
 	 * The maximum number of records that can be retrieved from a salesforce bulk
 	 * query at a time.
 	 */
-	public static final String SALESFORCE_MAX_RETRIES = "max.retries";
+	private static final String SALESFORCE_MAX_RETRIES = "max.retries";
 	/**
 	 * The default maximum number of records that can be retrieved from the bulk api
 	 * in one go
@@ -59,38 +60,38 @@ public class SalesforceConfigFragment extends ConfigFragment {
 	/**
 	 * The version of the API that the connector should use to run its queries
 	 */
-	public static final String SALESFORCE_API_VERSION = "salesforce.api.version";
+	private static final String SALESFORCE_API_VERSION = "salesforce.api.version";
 	private static final String SALESFORCE_API_VERSION_DEFAULT = "v66";
 
 	/**
 	 * The salesforce client secret for authentication
 	 */
-	public static final String SALESFORCE_CLIENT_SECRET = "salesforce.client.secret";
+	private static final String SALESFORCE_CLIENT_SECRET = "salesforce.client.secret";
 
 	/**
 	 * The salesforce client id for authentication
 	 */
-	public static final String SALESFORCE_CLIENT_ID = "salesforce.client.id";
+	private static final String SALESFORCE_CLIENT_ID = "salesforce.client.id";
 
 	/**
 	 * The salesforce username for authentication
 	 */
-	public static final String SALESFORCE_USERNAME = "salesforce.username";
+	private static final String SALESFORCE_USERNAME = "salesforce.username";
 
 	/**
 	 * The salesforce username for password
 	 */
-	public static final String SALESFORCE_PASSWORD = "salesforce.password";
+	private static final String SALESFORCE_PASSWORD = "salesforce.password";
 
 	/**
 	 * The salesforce organization uri for Bulk Api and pub sub queries
 	 */
-	public static final String SALESFORCE_URI = "salesforce.uri";
+	private static final String SALESFORCE_URI = "salesforce.uri";
 
 	/**
 	 * The salesforce OAUTH organization uri for password
 	 */
-	public static final String SALESFORCE_OAUTH_URI = "salesforce.oauth.uri";
+	private static final String SALESFORCE_OAUTH_URI = "salesforce.oauth.uri";
 
 	/**
 	 * Allows data to be added directly into the config fragment
@@ -131,13 +132,27 @@ public class SalesforceConfigFragment extends ConfigFragment {
 	}
 
 	/**
-	 *
+	 * Setter to configure configData
+	 * 
+	 * @param configData
+	 *            ConfigData is the map of properties and values to update
+	 * @return The Setter for the configData
+	 */
+	public static Setter setter(final Map<String, String> configData) {
+		return new Setter(configData);
+	}
+
+	/**
+	 * Add the salesforce connection configuration required to the configDef
+	 * 
 	 * @param configDef
+	 *            Add the Salesforce connection configuration required to the
+	 *            configDef
 	 */
 	static void addSalesforceConnectionDetails(final ConfigDef configDef) {
 		var salesforceGroupCounter = 0;
 		ExtendedConfigKey.builder(SALESFORCE_MAX_RECORDS).group(GROUP_SALESFORCE).orderInGroup(++salesforceGroupCounter)
-				.since("0.0.1").defaultValue(SALESFORCE_MAX_RECORDS_DEFAULT).type(ConfigDef.Type.LONG)
+				.since("1.0.0").defaultValue(SALESFORCE_MAX_RECORDS_DEFAULT).type(ConfigDef.Type.LONG)
 				.validator(ConfigDef.Range.between(1L, 15000L)).importance(ConfigDef.Importance.MEDIUM)
 				.documentation(
 						"Salesforce default maximum number of records to retrieve from the Bulk API. Must be at least 100 and at most 100000, default value is "
@@ -145,7 +160,7 @@ public class SalesforceConfigFragment extends ConfigFragment {
 				.width(ConfigDef.Width.NONE).build();
 
 		ExtendedConfigKey.builder(SALESFORCE_MAX_RETRIES).group(GROUP_SALESFORCE).orderInGroup(++salesforceGroupCounter)
-				.since("0.0.1").defaultValue(SALESFORCE_MAX_RETRIES_DEFAULT).type(ConfigDef.Type.LONG)
+				.since("1.0.0").defaultValue(SALESFORCE_MAX_RETRIES_DEFAULT).type(ConfigDef.Type.LONG)
 				.validator(ConfigDef.Range.between(1L, 5L)).importance(ConfigDef.Importance.MEDIUM)
 				.documentation(
 						"Salesforce default maximum number of retries against API. Must be at least 1 and at most 5, default value is "
@@ -153,7 +168,7 @@ public class SalesforceConfigFragment extends ConfigFragment {
 				.width(ConfigDef.Width.NONE).build();
 
 		ExtendedConfigKey.builder(SALESFORCE_API_VERSION).group(GROUP_SALESFORCE).orderInGroup(++salesforceGroupCounter)
-				.since("0.0.1").defaultValue(SALESFORCE_API_VERSION_DEFAULT).type(ConfigDef.Type.STRING)
+				.since("1.0.0").defaultValue(SALESFORCE_API_VERSION_DEFAULT).type(ConfigDef.Type.STRING)
 				.validator(new ConfigDef.NonEmptyString()).importance(ConfigDef.Importance.MEDIUM)
 				.documentation(
 						"API version of the Salesforce API to use when communicating with Salesforce, default value is "
@@ -162,38 +177,38 @@ public class SalesforceConfigFragment extends ConfigFragment {
 
 		// Salesforce authentication config
 		ExtendedConfigKey.builder(SALESFORCE_USERNAME).group(GROUP_SALESFORCE).orderInGroup(++salesforceGroupCounter)
-				.since("0.0.1").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
+				.since("1.0.0").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
 				.importance(ConfigDef.Importance.MEDIUM)
 				.documentation("Salesforce username that is used to authenticate over oauth with the api.")
 				.width(ConfigDef.Width.NONE).build();
 
 		ExtendedConfigKey.builder(SALESFORCE_PASSWORD).group(GROUP_SALESFORCE).orderInGroup(++salesforceGroupCounter)
-				.since("0.0.1").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
+				.since("1.0.0").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
 				.importance(ConfigDef.Importance.MEDIUM)
 				.documentation("Salesforce password that is used to authenticate over oauth with the api.")
 				.width(ConfigDef.Width.NONE).build();
 
 		ExtendedConfigKey.builder(SALESFORCE_CLIENT_ID).group(GROUP_SALESFORCE).orderInGroup(++salesforceGroupCounter)
-				.since("0.0.1").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
+				.since("1.0.0").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
 				.importance(ConfigDef.Importance.MEDIUM)
 				.documentation("Salesforce client id that is used to authenticate over oauth with the api.")
 				.width(ConfigDef.Width.NONE).build();
 
 		ExtendedConfigKey.builder(SALESFORCE_CLIENT_SECRET).group(GROUP_SALESFORCE)
-				.orderInGroup(++salesforceGroupCounter).since("0.0.1").type(ConfigDef.Type.STRING)
+				.orderInGroup(++salesforceGroupCounter).since("1.0.0").type(ConfigDef.Type.STRING)
 				.validator(new ConfigDef.NonEmptyString()).importance(ConfigDef.Importance.MEDIUM)
 				.documentation("Salesforce client secret that is used to authenticate over oauth with the api.")
 				.width(ConfigDef.Width.NONE).build();
 
 		ExtendedConfigKey.builder(SALESFORCE_URI).group(GROUP_SALESFORCE).orderInGroup(++salesforceGroupCounter)
-				.since("0.0.1").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
+				.since("1.0.0").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
 				.importance(ConfigDef.Importance.MEDIUM)
 				.documentation(
 						"Salesforce domain uri that is used to query the bulk api this is a uri specific to your organization and domain supplied by Salesforce.")
 				.width(ConfigDef.Width.NONE).build();
 
 		ExtendedConfigKey.builder(SALESFORCE_OAUTH_URI).group(GROUP_SALESFORCE).orderInGroup(++salesforceGroupCounter)
-				.since("0.0.1").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
+				.since("1.0.0").type(ConfigDef.Type.STRING).validator(new ConfigDef.NonEmptyString())
 				.importance(ConfigDef.Importance.MEDIUM)
 				.documentation(
 						"Salesforce oauth uri that is used to authenticate over oauth with the api, this is a uri specific to your organization and domain supplied by Salesforce.")
@@ -272,6 +287,110 @@ public class SalesforceConfigFragment extends ConfigFragment {
 	 */
 	public String getSalesforceOauthUri() {
 		return dataAccess.getString(SALESFORCE_OAUTH_URI);
+	}
+
+	/**
+	 * A setter for the SalesforceConfigFragment.
+	 */
+	public final static class Setter extends AbstractFragmentSetter<Setter> {
+
+		private Setter(final Map<String, String> data) {
+			super(data);
+		}
+
+		/**
+		 * Set the Username used for Oauth configuration
+		 * 
+		 * @param oAuthUserName
+		 *            The username used for Oauth authentication
+		 * @return The Oauth Salesforce username
+		 */
+		public Setter getOauthUsername(String oAuthUserName) {
+			return setValue(SALESFORCE_USERNAME, oAuthUserName);
+		}
+
+		/**
+		 * Set the Password used for Oauth configuration
+		 * 
+		 * @param oAuthPassword
+		 *            The password used for Oauth authentication
+		 * @return The Oauth Salesforce password
+		 */
+		public Setter getOauthPassword(String oAuthPassword) {
+			return setValue(SALESFORCE_PASSWORD, oAuthPassword);
+		}
+
+		/**
+		 * Set the Client Id used for Oauth configuration
+		 * 
+		 * @param clientId
+		 *            The clientId used for Oauth configuration
+		 * @return The Oauth Salesforce client Id
+		 */
+		public Setter getOauthClientId(String clientId) {
+			return setValue(SALESFORCE_CLIENT_ID, clientId);
+		}
+
+		/**
+		 * Set the Client Secret used for Oauth configuration
+		 * 
+		 * @param clientSecret
+		 *            the client secret used for authentication
+		 * @return The Oauth Salesforce client secret
+		 */
+		public Setter getOauthClientSecret(String clientSecret) {
+			return setValue(SALESFORCE_CLIENT_SECRET, clientSecret);
+		}
+
+		/**
+		 * Set the specific Salesforce uri used for all requests to the bulk api
+		 * 
+		 * @param salesforceUri
+		 *            A string representation of the uri to use with Salesforce
+		 * @return The target Salesforce Uri
+		 */
+		public Setter getSalesforceUri(String salesforceUri) {
+			return setValue(SALESFORCE_URI, salesforceUri);
+		}
+
+		/**
+		 * Set the Salesforce Api version to be returned
+		 * 
+		 * @param apiVersion
+		 *            A string that identifies the Salesforce apiVersion that the
+		 *            connector should execute against
+		 * @return The target Salesforce api version
+		 */
+		public Setter getSalesforceApiVersion(String apiVersion) {
+			return setValue(SALESFORCE_API_VERSION, apiVersion);
+		}
+
+		/**
+		 * Set the maximum number of records to return from the Bulk Api Query at a
+		 * time.
+		 * 
+		 * @param maxRecords
+		 *            An int representing the maximum number of records to retrieve from
+		 *            Salesforce at a time
+		 * @return An int with the maximum number of records to retrieve on each page of
+		 *         the Bulk Api.
+		 */
+		public Setter getSalesforceMaxRecords(int maxRecords) {
+			return setValue(SALESFORCE_MAX_RECORDS, maxRecords);
+		}
+
+		/**
+		 * Set the specific Salesforce uri used for all requests including
+		 * authentication and submitting queries
+		 * 
+		 * @param salesforceOauthUri
+		 *            A string representation of the oauth uri to use with Salesforce
+		 * @return The target Salesforce OAUTH Uri
+		 */
+		public Setter getSalesforceOauthUri(String salesforceOauthUri) {
+			return setValue(SALESFORCE_OAUTH_URI, salesforceOauthUri);
+		}
+
 	}
 
 }
